845a7ab69c08d436827c06643d7b44de9098789a
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-04-27 13:11:44 +0300

    add new code

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_login import login_required, login_user, logout_user, current_user
 from app.models import User
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
-
+#добавляем возможность редактирования профиля
 
 
 @app.route('/')
@@ -46,6 +46,36 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def update():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+#проверяем, что имя пользователя не занято
+        if form.username.data != current_user.username:
+            user = User.query.filter_by(username=form.username.data).first()
+            if user:
+                flash('Это имя занято. Попробуйте другое.', 'danger')
+                return render_template('edit_profile.html', form=form)
+
+#проверяем, что email не занят
+        if form.email.data != current_user.email:
+            user = User.query.filter_by(email=form.email.data).first()
+            if user:
+                flash('Этот email занят. Попробуйте другой.', 'danger')
+                return render_template('edit_profile.html', form=form)
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Ваш аккаунт был обновлен', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('edit_profile.html', form=form)
+
 
 @app.route('/account')
 @login_required
